3ae88330ffaf9da8a33e0278d9f760d50b1a71d4
Michał Krysztofik <michal.krysztofik@kontomatik.com>
2025-04-23 10:23:18 +0200

    Add basic map

--- src/main/java/com/example/demo/MainView.java
+++ src/main/java/com/example/demo/MainView.java
@@ -7,6 +7,13 @@ import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import software.xdev.vaadin.maps.leaflet.MapContainer;
+import software.xdev.vaadin.maps.leaflet.basictypes.LLatLng;
+import software.xdev.vaadin.maps.leaflet.layer.raster.LTileLayer;
+import software.xdev.vaadin.maps.leaflet.layer.ui.LMarker;
+import software.xdev.vaadin.maps.leaflet.map.LMap;
+import software.xdev.vaadin.maps.leaflet.registry.LComponentManagementRegistry;
+import software.xdev.vaadin.maps.leaflet.registry.LDefaultComponentManagementRegistry;
 
 /**
  * The main view contains a button and a click listener.
@@ -14,27 +21,62 @@ import com.vaadin.flow.router.Route;
 @Route("")
 public class MainView extends VerticalLayout {
 
-    public MainView() {
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
+  public MainView() {
+    demoGreet();
+    showMap();
+  }
 
-        // Button click listeners can be defined as lambda expressions
-        GreetService greetService = new GreetService();
-        Button button = new Button("Say hello", e ->  {
-            add(new Paragraph(greetService.greet(textField.getValue())));
-        });
+  private void demoGreet() {
+    // Use TextField for standard text input
+    TextField textField = new TextField("Your name");
 
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button is more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+    // Button click listeners can be defined as lambda expressions
+    var greeting = new Paragraph();
+    GreetService greetService = new GreetService();
+    Button button = new Button("Say hello", e -> {
+      greeting.setText(greetService.greet(textField.getValue()));
+    });
 
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
+    // Theme variants give you predefined extra styles for components.
+    // Example: Primary button is more prominent look.
+    button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-        addClassName("centered-content");
+    // You can specify keyboard shortcuts for buttons.
+    // Example: Pressing enter in this view clicks the Button.
+    button.addClickShortcut(Key.ENTER);
 
-        add(textField, button);
-    }
+    // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
+    addClassName("centered-content");
+
+    add(textField, button, greeting);
+  }
+
+  private void showMap() {
+    // Let the view use 100% of the site
+    setSizeFull();
+
+    // Create the registry which is needed so that components can be reused and their methods invoked
+    // Note: You normally don't need to invoke any methods of the registry and just hand it over to the components
+    LComponentManagementRegistry reg = new LDefaultComponentManagementRegistry(this);
+
+    // Create and add the MapContainer (which contains the map) to the UI
+    var mapContainer = new MapContainer(reg);
+    mapContainer.setSizeFull();
+    add(mapContainer);
+
+    LMap map = mapContainer.getlMap();
+
+    // Add a (default) TileLayer so that we can see something on the map
+    map.addLayer(LTileLayer.createDefaultForOpenStreetMapTileServer(reg));
+
+    // Set what part of the world should be shown
+    map.setView(new LLatLng(reg, 52.254779538366435, 20.998446764397098), 16);
+
+    // Create a new marker
+    new LMarker(reg, new LLatLng(reg, 52.254779538366435, 20.998446764397098))
+      // Bind a popup which is displayed when clicking the marker
+      .bindPopup("Kontomatik")
+      .addTo(map);
+  }
+
 }
