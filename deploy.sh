2f6f28892d469497ba184a484b27a7b8a262f086
Michał Krysztofik <michal.krysztofik@kontomatik.com>
2025-07-16 09:33:17 +0200

    Add static IP

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,24 @@
 # Create a VM then deploy your application to the VM
 # https://spring-gcp.saturnism.me/getting-started/helloworld/compute-engine
 
+#gcloud compute addresses delete geocaching-webapp --region=europe-central2 --project=silken-glyph-365213 --quiet
+gcloud compute addresses create geocaching-webapp \
+    --region=europe-central2 \
+    --network-tier=STANDARD \
+    --project=silken-glyph-365213
+STATIC_IP=$(gcloud compute addresses list --filter="name=geocaching-webapp" --format="value(address)" --project=silken-glyph-365213)
+
 ./gradlew clean bootJar
-JAR_PATH=build/libs/geocaching-webapp.jar
 # `gcloud services enable compute.googleapis.com` if not enabled
 gcloud compute instances stop geocaching-webapp --zone=europe-central2-c --project=silken-glyph-365213
 # deleting old instance may take some time
 gcloud compute instances delete geocaching-webapp --zone=europe-central2-c --project=silken-glyph-365213 --quiet
-gcloud compute instances create geocaching-webapp --scopes=cloud-platform --zone=europe-central2-c --project=silken-glyph-365213
+gcloud compute instances create geocaching-webapp \
+--scopes=cloud-platform \
+--machine-type="e2-medium" \
+--network-interface "network-tier=STANDARD,address=geocaching-webapp" \
+--zone=europe-central2-c \
+--project=silken-glyph-365213
 gcloud compute instances add-tags geocaching-webapp --tags=webapp --zone=europe-central2-c --project=silken-glyph-365213
 # will return error if rule already exist - can be ignored
 gcloud compute firewall-rules create webapp-rule \
@@ -17,8 +28,8 @@ gcloud compute firewall-rules create webapp-rule \
   --allow=tcp:8080 \
   --project=silken-glyph-365213
 # need to wait a while before VM starts accepting connection
-sleep 10
-gcloud compute scp $JAR_PATH geocaching-webapp:~/geocaching-webapp.jar --zone=europe-central2-c --project=silken-glyph-365213
+sleep 15
+gcloud compute scp build/libs/geocaching-webapp.jar geocaching-webapp:~/geocaching-webapp.jar --zone=europe-central2-c --project=silken-glyph-365213
 
 gcloud compute ssh geocaching-webapp --zone=europe-central2-c --project=silken-glyph-365213 \
   --command="sudo apt-get update && sudo apt-get install -y openjdk-17-jdk && nohup java -jar geocaching-webapp.jar &" &
