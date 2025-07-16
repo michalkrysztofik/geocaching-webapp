c02d8e095d70f7a92515dbb970c78be147a3115e
Michał Krysztofik <michal.krysztofik@kontomatik.com>
2025-07-16 11:27:43 +0200

    Use own authorization

--- src/main/java/com/example/demo/LoginView.java
+++ src/main/java/com/example/demo/LoginView.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
-import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
@@ -9,14 +12,26 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 @Route("login")
 @PageTitle("Login")
 @AnonymousAllowed
-public class LoginView extends VerticalLayout {
-  // URL that Spring Security uses to connect to Google services
-  private static final String OAUTH_URL = "/oauth2/authorization/google";
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+
+  private final LoginForm login = new LoginForm();
 
   public LoginView() {
-    Anchor loginLink = new Anchor(OAUTH_URL, "Login with Google");
-    // Instruct Vaadin Router to ignore doing SPA handling
-    loginLink.setRouterIgnore(true);
-    add(loginLink);
+    addClassName("login-view");
+    setSizeFull();
+
+    setJustifyContentMode(JustifyContentMode.CENTER);
+    setAlignItems(Alignment.CENTER);
+
+    login.setAction("login");
+
+    add(new H1("Geocaching WebApp"), login);
   }
+
+  @Override
+  public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+    if (beforeEnterEvent.getLocation().getQueryParameters().getParameters().containsKey("error"))
+      login.setError(true);
+  }
+
 }
--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,23 +1,56 @@
 package com.example.demo;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends VaadinWebSecurity {
-  // https://vaadin.com/blog/oauth-2-and-google-sign-in-for-a-vaadin-application
-
-  private static final String LOGIN_URL = "/login";
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    // Delegating the responsibility of general configurations
+    // of http security to the super class. It's configuring
+    // the followings: Vaadin's CSRF protection by ignoring
+    // framework's internal requests, default request cache,
+    // ignoring public views annotated with @AnonymousAllowed,
+    // restricting access to other views/endpoints, and enabling
+    // NavigationAccessControl authorization.
+    // You can add any possible extra configurations of your own
+    // here (the following is just an example):
+
+    // http.rememberMe().alwaysRemember(false);
+
+    // Configure your static resources with public access before calling
+    // super.configure(HttpSecurity) as it adds final anyRequest matcher
+    http.authorizeHttpRequests(auth -> auth
+      .requestMatchers("/public/**")
+      .permitAll());
+
     super.configure(http);
-    http.oauth2Login().loginPage(LOGIN_URL).permitAll();
-    //http.oauth2Login(Customizer.withDefaults()).loginPage(LOGIN_URL).permitAll();
+
+    setLoginView(http, LoginView.class);
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    super.configure(web);
+  }
+
+  @Bean
+  public UserDetailsManager userDetailsService() {
+    UserDetails user = org.springframework.security.core.userdetails.User.withUsername("user")
+      .password("{noop}1234")
+      .roles("USER")
+      .build();
+    return new InMemoryUserDetailsManager(user);
   }
 
 }
